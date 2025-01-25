98b8957973314d99960f14af5a5900e094ffdad9
Rinty <fonoji8439@javnoi.com>
2025-01-26 04:59:31 +0800

    Add files via upload

---
+++ Scripts/00 ThemeInfo.lua
@@ -0,0 +1,19 @@
+-- theme identification file
+
+themeInfo = {
+	Name = "Blue Paradise",
+	Version = "1.11",
+	Date = "20250122"
+}
+
+function getThemeName()
+	return themeInfo["Name"]
+end
+
+function getThemeVersion()
+	return themeInfo["Version"]
+end
+
+function getThemeDate()
+	return themeInfo["Date"]
+end

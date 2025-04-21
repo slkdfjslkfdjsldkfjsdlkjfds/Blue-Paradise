52055cf2b8634a86e0bc4072a7a89fc452f4c9d5
slkdfjslkfdjsldkfjsdlkjfds <madopolus@gmail.com>
2025-04-21 13:51:07 +0800

    Add files via upload

---
+++ Scripts/00 ThemeInfo.lua
@@ -0,0 +1,19 @@
+-- theme identification file
+
+themeInfo = {
+	Name = "'Blue Paradise",
+	Version = "idk you tell me",
+	Date = "20250402"
+}
+
+function getThemeName()
+	return themeInfo["Name"]
+end
+
+function getThemeVersion()
+	return themeInfo["Version"]
+end
+
+function getThemeDate()
+	return themeInfo["Date"]
+end

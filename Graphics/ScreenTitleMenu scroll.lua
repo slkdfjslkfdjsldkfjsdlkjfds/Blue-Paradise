fc67fa2dbcd3e15fb2a069ad8a89a2816d8fc97f
slkdfjslkfdjsldkfjsdlkjfds <madopolus@gmail.com>
2025-04-21 13:52:17 +0800

    Add files via upload

---
+++ Graphics/ScreenTitleMenu scroll.lua
@@ -0,0 +1,17 @@
+local gc = Var("GameCommand")
+
+return Def.ActorFrame {
+	LoadFont("Common Normal") ..
+		{
+			Text = THEME:GetString("ScreenTitleMenu", gc:GetText()),
+			OnCommand = function(self)
+				self:xy(300, -32)
+			end,
+			GainFocusCommand = function(self)
+				self:zoom(0.61):diffuse(getMainColor('positive'))
+			end,
+			LoseFocusCommand = function(self)
+				self:zoom(0.55):diffuse(getTitleColor('Line_Right'))
+			end
+		}
+}
